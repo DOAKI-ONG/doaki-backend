12fb257ef8dcdf9d5610e0989db992e900c7b7fd
ThulioBezerra <thulioalves20@gmail.com>
2025-05-07 14:40:23 -0300

    feat: add rout, controlle4r and repository to patch itens on user

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,13 +3,17 @@ import z from "zod";
 import UserRepository from "../repositories/UserRepository";
 import jsonwebtoken from "jsonwebtoken";
 import dotenv from "dotenv";
+import { encryptPassword } from "src/services/users/encryptPassword";
+import { checkPassword } from "src/services/users/checkPassword";
 
 dotenv.config();
 export default class UserController {
   static async loginUser(req: Request, res: Response) {
     const loginUserSchema = z.object({
       email: z.string().email("Email com formato inválido"),
-      password: z.string().min(8, "A senha deve conter pelo menos 8 caracteres"),
+      password: z
+        .string()
+        .min(8, "A senha deve conter pelo menos 8 caracteres"),
     });
     try {
       loginUserSchema.parse(req.body);
@@ -23,19 +27,20 @@ export default class UserController {
     }
     const { email, password } = req.body;
     const userExists = await UserRepository.findByEmail(email);
-    if (!userExists) {
+    if (!userExists || !userExists.status) {
       return res.status(404).json({
         message: "Usuário não encontrado",
       });
     }
-    if (userExists.password !== password) {
-      return res.status(401).json({
-        message: "Senha incorreta",
-      });
+    const password_encrypted = userExists.password || "";
+
+    const check_password = await checkPassword(password, password_encrypted);
+    if (!check_password) {
+      return res.status(403).send("Senha inválida!");
     }
     const token = jsonwebtoken.sign(
       {
-        id: userExists?.id,
+        id: userExists?.id_user,
       },
       String(process.env.SECRET),
       {
@@ -72,11 +77,12 @@ export default class UserController {
       const { first_name, last_name, email, password } =
         registerUserSchema.parse(req.body);
       const name = `${first_name} ${last_name}`;
+      const password_encrypted = await encryptPassword(password);
       await UserRepository.create({
         name,
         email,
-        password,
-        type: "CLIENT",
+        password: password_encrypted,
+        type: "DONATOR",
       });
       return res.status(201).json({
         message: "Usuário criado com sucesso",
@@ -124,4 +130,51 @@ export default class UserController {
       });
     }
   }
+  static async deleteUser(req: Request, res: Response) {
+    const id = res.locals.id;
+    try {
+      const user = await UserRepository.findById(id);
+      if (!user) {
+        return res.status(404).json({
+          message: "Usuário não encontrado",
+        });
+      }
+      await UserRepository.delete(id);
+    } catch (error) {
+      return res.status(500).json({
+        message: "Erro interno do servidor",
+      });
+    }
+    return res.status(200).json({
+      message: "Usuário deletado com sucesso",
+    });
+  }
+  static async updateUser(req: Request, res: Response) {
+    const updateUserSchema = z.object({
+      email: z.string().email("Email com formato inválido").optional(),
+      phone: z.string().optional(),
+    });
+    const id  = res.locals.id;
+    const { email, phone } = updateUserSchema.parse(req.body);
+    const userExists = await UserRepository.findById(id);
+    if (!userExists) {
+      return res.status(404).json({
+        message: "Usuário não encontrado",
+      });
+    }
+    try {
+      const user = await UserRepository.update(id, {
+        email,
+        phone,
+      });
+      return res.status(200).json({
+        message: "Usuário atualizado com sucesso"
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: "Erro interno do servidor",
+      });
+    }
 }
+}
+
