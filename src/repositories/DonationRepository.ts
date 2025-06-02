41b73ceda280f06ddd88e33c514b79d7721ffbac
ThulioBezerra <thulioalves20@gmail.com>
2025-06-02 12:27:42 -0300

    feat: add a create donation route and logic.

--- src/repositories/DonationRepository.ts
+++ src/repositories/DonationRepository.ts
@@ -1,18 +1,57 @@
+import { createPaymentClient } from "@lib/mercadopago";
+import { prisma } from "@lib/prisma";
+
 export default class DonationRepository {
-    static async create(data: {
-        id_user_fk: string;
-        id_ong_fk: string;
-        transaction_id: string;
-        transaction_amount: number;
-        transaction_status: string;
-        transaction_type: string;
-        payment_method_id: string;
-        payment_method_type: string;
-        card_brand: string;
-        card_expiration_month: number;
-        card_expiration_year: number;
-        card_last_four_digits: string;
-    }) {
-        // Implement the logic to create a donation in the database
+  static async create(cnpj: string, paymentData: any, idUser: string) {
+    const ongExists = await prisma.ong.findFirst({
+      where: {
+        cnpj: cnpj,
+      },
+    });
+    const body = paymentData.paymentData;
+    const bodyPayment = {
+      token: body.token || undefined,
+      issuer_id: body.issuer_id || undefined,
+      payment_method_id: body.payment_method_id,
+      transaction_amount: parseFloat(body.transaction_amount),
+      installments: body.installments || undefined,
+      payer: {
+        email: body.payer.email,
+        identification: {
+          type: body.payer.identification.type,
+          number: body.payer.identification.number,
+        },
+      },
+    };
+
+    if (!ongExists) {
+      throw new Error("ONG not found");
     }
+    const payment = createPaymentClient(ongExists?.accessToken!);
+    const paymentResponse = await payment.create({ body: bodyPayment });
+    const {
+      id,
+      status_detail,
+      date_created,
+      date_approved,
+      payment_type_id,
+      transaction_amount,
+    } = paymentResponse;
+    const paymentCreated = await prisma.donation.create({
+      data: {
+        userId: idUser,
+        amount: transaction_amount,
+        ongId: ongExists.id_ong,
+        paymentMpId: id,
+        status: status_detail,
+        dataCreated: date_created,
+        dataApproved: date_approved,
+        paymentMethod: payment_type_id,
+      },
+    });
+    if (!paymentCreated) {
+      throw new Error("Failed to create donation");
+    }
+    return paymentCreated;
+  }
 }
