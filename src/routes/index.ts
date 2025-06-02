b91fe73208d280a474e4d8f606d075d1c9c7c0ff
ThulioBezerra <thulioalves20@gmail.com>
2025-06-02 15:46:54 -0300

    add ongpublic route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,11 @@ import UserPublicRoutes from "./public/UserPublicRoutes";
 import UserPrivateRoutes from "./private/UserPrivateRoutes";
 import OngPrivateRoutes from "./private/OngPrivateRoute";
 import DonationPrivateRoutes from "./private/DonationPrivateRoute";
+import OngPublicRoutes from "./public/OngPublicRoute";
+
 const router = express.Router();
 //falta colocar se o status do user for false...
 router.use("/users", UserPublicRoutes, UserPrivateRoutes);
-router.use("/ongs", OngPrivateRoutes);
+router.use("/ongs", OngPrivateRoutes, OngPublicRoutes);
 router.use("/donations", DonationPrivateRoutes);
 export default router;
