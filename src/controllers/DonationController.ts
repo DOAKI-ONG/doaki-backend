0ba268a5eeb3026b529ec1a2b914ea099ba43b6a
ThulioBezerra <thulioalves20@gmail.com>
2025-06-03 12:11:36 -0300

    style: apply formatting code and alter payment.

--- src/controllers/DonationController.ts
+++ src/controllers/DonationController.ts
@@ -1,11 +1,15 @@
-import DonationRepository from '@repositories/DonationRepository';
-import { Request, Response } from 'express';
+import DonationRepository from "@repositories/DonationRepository";
+import { Request, Response } from "express";
 
 export default class DonationController {
-  static async createDonation(req: Request, res: Response, validatedBody: any) {
+  static async createDonation(req: Request, res: Response) {
     const { cnpj } = req.params;
-    const paymentData  = req.body;
-    const payment = await DonationRepository.create(cnpj, paymentData, res.locals.id);
+    const paymentData = req.body;
+    const payment = await DonationRepository.create(
+      cnpj,
+      paymentData,
+      res.locals.id
+    );
     return res.status(201).json(payment);
   }
   static async paymentUpdateCard(req: Request, res: Response) {}
