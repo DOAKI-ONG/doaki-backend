792c6db53f5a9e56a7e0cf80ffbad0fe0c0713d1
ThulioBezerra <thulioalves20@gmail.com>
2025-06-03 15:44:46 -0300

    feat: add body for payment request.

--- src/controllers/DonationController.ts
+++ src/controllers/DonationController.ts
@@ -1,10 +1,11 @@
 import DonationRepository from "@repositories/DonationRepository";
 import { Request, Response } from "express";
+import { PaymentCreateRequest } from "src/types/paymentBody.types";
 
 export default class DonationController {
   static async createDonation(req: Request, res: Response) {
     const { cnpj } = req.params;
-    const paymentData = req.body;
+    const paymentData = req.body as PaymentCreateRequest;
     const payment = await DonationRepository.create(
       cnpj,
       paymentData,
